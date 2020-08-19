51dae33f48bc2a9b91b61dd3d95777377f8f48ba
Rakshith Poojary <rakshithp7@gmail.com>
2020-08-19 20:24:34 +0530

    Updates

---
+++ precache-manifest.cc341ab675ca156d3b165deb80c49ba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1d7c9eb708678eecc8e82af6d7d9ce",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "b548c7da05dcaf537800",
+    "url": "/react-covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "461505e323437d03eff9",
+    "url": "/react-covid-tracker/static/css/main.dd229012.chunk.css"
+  },
+  {
+    "revision": "b548c7da05dcaf537800",
+    "url": "/react-covid-tracker/static/js/2.c318c7c7.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/react-covid-tracker/static/js/2.c318c7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461505e323437d03eff9",
+    "url": "/react-covid-tracker/static/js/main.9d8f89ff.chunk.js"
+  },
+  {
+    "revision": "91b8d8b30a48a12b291e",
+    "url": "/react-covid-tracker/static/js/runtime-main.ab7b88d2.js"
+  }
+]);
